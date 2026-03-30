42ac1b9c5ab9c926235e2e42220f91da528b2b7c
Nandini Singh <nandinisingh11426@gmail.com>
2026-03-30 14:59:57 +0530

    Add files via upload

---
+++ NANDINI 7/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    int id;
+    String name;
+    Student(int i, String n){
+        id = i;
+        name = n;
+    }
+    void display(){
+        System.out.println("id =" + id);
+        System.out.println ("name = " + name);
+    }
+    public static void main(String args[]){
+        Student s1 = new Student(101 ,"rahul");
+        s1.display();
+    }  
+}
---
+++ NANDINI 7/TemperatureConverter.java
@@ -0,0 +1,13 @@
+public class TemperatureConverter {
+    double celsius;
+    void convert(){
+    double fahrenheit=(celsius*9/5)+32;
+    System.out.println("Temperature in fahrenheit="+fahrenheit);
+}
+public static void main(String[] args) {
+    TemperatureConverter obj=new TemperatureConverter();
+    obj.celsius=37;
+    obj.convert();
+}
+}
+
